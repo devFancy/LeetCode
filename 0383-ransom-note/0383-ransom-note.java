12123b2cdabfb5ea102cc83a948a69e3f8dd3087
Junyong Moon <fancy.junyongmoon@gmail.com>
2024-09-17 15:01:01 +0900

    Time: 10 ms (57.96%), Space: 45.3 MB (20.21%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+       
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        int len = magazine.length();
+        
+        for(int i = 0; i < len; i++) {
+            char current = magazine.charAt(i);
+            map.put(current, map.getOrDefault(current, 0) + 1);
+        }
+        
+        len = ransomNote.length();
+        
+        for(int i = 0; i < len; i++) {
+            char key = ransomNote.charAt(i);
+            if(map.get(key) != null && map.get(key) > 0) {
+                map.put(key, map.get(key) - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
