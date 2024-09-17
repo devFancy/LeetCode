9bf0244193332202695ab2302a32666870d77afd
Junyong Moon <fancy.junyongmoon@gmail.com>
2024-09-17 14:31:29 +0900

    Time: 24 ms (98.05%), Space: 40 MB (92.46%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -9,14 +9,14 @@ public class Solution extends VersionControl {
         int mid = 0;
         
         while(start < end) {
-            mid = start + (end - start) / 2;
-            if(isBadVersion(mid)) { // 현재 버전(mid)이 잘못된 제품인 경우
-                end = mid; // mid가 최초의 잘못된 제품이거나, mid 이전에 잘못된 제품이 있을수도 있음
+            mid = start + (end - start) /2;
+            if(isBadVersion(mid)) { // If Currently version is fail,
+                end = mid;
             }
-            else { // 현재 버전(mid)이 올바른 제품인 경우 
-                start = mid + 1; // mid 이후에 잘못된 버전이 있음
+            else  { // If Cunrrently version is true
+                start = mid + 1; // 
             }
         }
-        return start; // start == mid 일때 반복문을 탈출한다. 
+        return start;
     }
 }
