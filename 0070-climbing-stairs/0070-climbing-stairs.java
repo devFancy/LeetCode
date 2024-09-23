f2228e1d6f92b7478191b3a4071ad68cf1033629
Junyong Moon <fancy.junyongmoon@gmail.com>
2024-09-23 18:23:14 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (53.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,16 +2,17 @@ class Solution {
     public int climbStairs(int n) {
         
 			if (n <= 1) {
-					return 1;
+	        return 1;
 			}
 			
-			int[] dp = new int[n+1];
-			dp[1] = 1;
-			dp[2] = 2;
+			int prev1 = 1;
+			int prev2 = 2;
 			
 			for(int i = 3; i <= n; i++) {
-				dp[i] = dp[i-1] + dp[i-2];
+				int newValue = prev1 + prev2;
+				prev1 = prev2;
+				prev2 = newValue;
 			}
-			return dp[n];
+			return prev2;
     }
 }
