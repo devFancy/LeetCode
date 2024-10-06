f17015a7629a05d302148c1cbca72cdfcedd6653
Junyong Moon <fancy.junyongmoon@gmail.com>
2024-10-06 15:27:11 +0900

    Time: 1 ms (100.00%), Space: 41.9 MB (30.88%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,24 +1,46 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int answer = 0;
-        HashMap<String, Integer> hm = new HashMap<>();
-        boolean odd = false;
-        for (String str : s.split("")) {
-            hm.put(str, hm.getOrDefault(str, 0) + 1);
+        // 문자 빈도수를 저장할 배열
+        int [] charCount = new int[128];
+
+        // 문자열의 각 문자 빈도수 계산
+        for(char c : s.toCharArray()) {
+            charCount[c]++;
         }
 
-        for (String str : hm.keySet()) {
-            if (hm.get(str) % 2 == 0) {
-                answer += hm.get(str);
-            } else {
-                if (odd == true) {
-                    if(hm.get(str) > 1) answer += hm.get(str) - 1;
-                } else {
-                    answer += hm.get(str);
-                    odd = true;
-                }
+        // return 회전문 길이
+        int longestPalindromeLength = 0;
+        // 홀수 여부 확인
+        boolean hasOddFrequency = false;
+
+        // 각 문자의 빈도수를 확인하여 회문 길이 계산
+        for (int count: charCount) {
+            if(count % 2 == 0) {
+                longestPalindromeLength += count;
+            }
+            else {
+                longestPalindromeLength += count -1;
+                hasOddFrequency = true;
             }
         }
-        return answer;
+
+        // 홀수 개수의 문자가 하나라도 있으면 중앙에 하나를 배치함
+        if (hasOddFrequency) {
+            longestPalindromeLength += 1;
+        }
+
+        return longestPalindromeLength;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // 예제 1
+        String s1 = "abccccdd";
+        System.out.println("Input: " + s1 + " => Output: " + solution.longestPalindrome(s1)); // Output: 7
+
+        // 예제 2
+        String s2 = "aba";
+        System.out.println("Input: " + s2 + " => Output: " + solution.longestPalindrome(s2)); // Output: 3
     }
 }
