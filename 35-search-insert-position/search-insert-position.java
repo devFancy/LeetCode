33caca37d6c2ed9ccbc4395f054702f720f67afe
Junyong Moon <fancy.junyongmoon@gmail.com>
2025-07-22 23:11:44 +0900

    Time: 0 ms (100.00%) | Memory: 43.3 MB (18.42%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,16 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0; 
-        int right = nums.length - 1; // 배열의 마지막 인덱스
+        int left = 0;
+        int right = nums.length - 1;
 
-        while(left <= right) {
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if (nums[mid] > target) {
-                right = mid - 1;
+            if (nums[mid] == target) {
+                return mid;
             } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
-                return mid;
+                right = mid -1;
             }
         }
         return left;
